0f4328e42bf3699f1bda2b6e308a348fae7516b4
DevTechProfile <devtechprofile@hotmail.com>
2021-02-09 18:54:32 +0100

    initial setup

---
+++ CoreTunerX/CoreTunerX/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace ReadEventLogExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventLog eventLog = new EventLog
+            {
+                Log = "System"
+            };
+
+            int threadCount = Environment.ProcessorCount;
+
+            try
+            {
+                var filteredEntries = eventLog.Entries
+                    .Cast<EventLogEntry>()
+                    .Where(logEntry => logEntry.InstanceId == 55)
+                    .Take(threadCount)
+                    .OrderBy(logEntry => Convert.ToInt32(logEntry.ReplacementStrings[1]))
+                    .ToList();
+
+                using (StreamWriter file =
+                    new StreamWriter("results.txt"))
+                {
+                    for (int i = 0; i < filteredEntries.Count; i++)
+                    {
+                        file.WriteLine($"Core {filteredEntries[i].ReplacementStrings[1]} with performance number {filteredEntries[i].ReplacementStrings[5]}");
+                    }
+                }
+
+                Process.Start("results.txt");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.WriteLine("Press any key...");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
